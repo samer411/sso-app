589bdff2e7b94961854e32591b4256c31bda314d
samer411 <samer.nassim.behery@gmail.com>
2022-07-30 17:16:09 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sso-app';
+  constructor(private OAuthService: OAuthService) {
+    this.configurationSingleSignOn();
+  }
+  configurationSingleSignOn() {
+    this.OAuthService.configure(authCodeFlowConfig);
+    this.OAuthService.tokenValidationHandler = new JwksValidationHandler();
+    this.OAuthService.loadDiscoveryDocumentAndTryLogin();
+  }
+  login() {
+    this.OAuthService.initImplicitFlow();
+  }
+  logout() {
+    this.OAuthService.logOut();
+  }
+  get token() {
+    let claims: any = this.OAuthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
